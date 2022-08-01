e413354aac663147d18756611e01b4a0fb7b9f5e
abderrahmane <abderrahamane7@gmail.com>
2022-08-01 09:51:04 +0100

    moved logic from (& deleted) App component to Quiz component

--- force-app/main/default/lwc/quiz/quiz.js
+++ force-app/main/default/lwc/quiz/quiz.js
@@ -1,13 +1,108 @@
-import { LightningElement, api, track } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class Quiz extends LightningElement {
-  @api questions;
   @track idx = 0;
   @track question = {};
+
+  dataIsLoaded = false;
+  isLoading = true;
+  quizId;
   end = false;
 
+  questions = [
+    {
+      id: "question1",
+      explanation: "Question one",
+      duration: 5,
+      points: 10,
+      options: [
+        {
+          id: "q1op1",
+          value: "Option one"
+        },
+        {
+          id: "q1op2",
+          value: "Option two"
+        },
+        {
+          id: "q1op3",
+          value: "Option three"
+        },
+        {
+          id: "q1op4",
+          value: "Option four"
+        }
+      ]
+    },
+    {
+      id: "question2",
+      explanation: "Question two",
+      duration: 10,
+      points: 10,
+      options: [
+        {
+          id: "q2op1",
+          value: "Option one"
+        },
+        {
+          id: "q2op2",
+          value: "Option two"
+        },
+        {
+          id: "q2op3",
+          value: "Option three"
+        },
+        {
+          id: "q2op4",
+          value: "Option four"
+        }
+      ]
+    },
+    {
+      id: "question3",
+      explanation: "Question three",
+      duration: 20,
+      points: 10,
+      options: [
+        {
+          id: "q3op1",
+          value: "Option one"
+        },
+        {
+          id: "q3op2",
+          value: "Option two"
+        },
+        {
+          id: "q3op3",
+          value: "Option three"
+        },
+        {
+          id: "q3op4",
+          value: "Option four"
+        }
+      ]
+    }
+  ];
+
   connectedCallback() {
-    this.question = this.questions[this.idx];
+    this.getQuizId();
+  }
+
+  getQuizId() {
+    /* eslint-disable @lwc/lwc/no-async-operation */
+    setTimeout(() => {
+      const url = new URL(window.location.href);
+      this.quizId = url.searchParams.get("quiz");
+      if (this.quizId) {
+        console.log(this.quizId);
+        this.isLoading = false;
+        this.dataIsLoaded = true;
+        this.question = this.questions[this.idx];
+      } else {
+        this.isLoading = false;
+        this.dataIsLoaded = false;
+      }
+    }, 2000);
   }
 
   next() {
