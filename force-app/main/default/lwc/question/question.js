5763a4e7fb29590753c9e954f813ce296ed89d6e
abderrahmane <abderrahamane7@gmail.com>
2022-08-04 16:35:39 +0100

    fixed the index of the question

--- force-app/main/default/lwc/question/question.js
+++ force-app/main/default/lwc/question/question.js
@@ -2,12 +2,19 @@ import { LightningElement, api } from "lwc";
 
 export default class Question extends LightningElement {
   @api question = {};
-  @api idx;
   @api length;
   options = [];
   timeout;
   answers = [1, 2, 3];
   hasRendered = false;
+  _idx;
+  @api
+  get idx() {
+    return this._idx;
+  }
+  set idx(value) {
+    this._idx = value + 1;
+  }
 
   renderedCallback() {
     if (!this.hasRendered) {
